f50c014138da5adf534a8a9ba947e5a6490051c1
Brianna Vetter <bri.vetter@gmail.com>
2022-04-01 12:42:19 -0400

    a8 REST sp22

--- src/components/tuiter/tuit-list/tuit-list-item.js
+++ src/components/tuiter/tuit-list/tuit-list-item.js
@@ -61,22 +61,7 @@ const TuitListItem = ({
 
                 </div>
 
-                <div className = "fa-pull-right">
-                         <div className="d-inline mx-2" >
-                             Likes: {tuit.likes}
-                             <i onClick={() => updateTuit(dispatch, {
-                               ...tuit,
-                               likes: tuit.likes + 1
-                             })} className="far fa-thumbs-up ms-2 "></i>
-                         </div>
-                         <div className="d-inline " >
-                             Dislikes: {tuit.dislikes}
-                              <i onClick={() => updateTuit(dispatch, {
-                                ...tuit,
-                                dislikes: tuit.dislikes + 1
-                              })} className="far fa-thumbs-down ms-2"></i>
-                          </div>
-                </div>
+
 
             </div>
 
--- src/components/tuiter/tuit-list/tuit-stats.js
+++ src/components/tuiter/tuit-list/tuit-stats.js
@@ -1,12 +1,14 @@
 import {useDispatch} from "react-redux";
 import {updateTuit,} from "../actions/tuits-actions";
 
+
+
 const TuitStats = ({tuit}) => {
   const dispatch = useDispatch();
   //const likeTuit = () => {
   //  dispatch({type: 'like-tuit', tuit});
-  //};
-if (tuit.liked === false){
+
+//if (tuit.liked === false){
   return (
     <div className ="mx-auto wd-icons">
         <span className="wd-first">
@@ -18,56 +20,23 @@ if (tuit.liked === false){
              {tuit.retuits}
         </span>
 
-        <span className="wd-third" onClick={() => updateTuit(dispatch, {
-                                                  ...tuit,
-                                                  likes: tuit.likes + 1,
-                                                  liked : true})}>
-        {
-          tuit.liked &&
-          <i className=" fas fa-heart me-1"
-             style={{color: 'red'}}></i>
-        }
-        {
-          !tuit.liked &&
-          <i className="far fa-heart me-1"></i>
-        }
-        {tuit && tuit.likes}
+        <span className="wd-third">
+             <div className="d-inline mx-2" >
+                 {tuit.likes}
+                 <i onClick={() => updateTuit(dispatch, {
+                   ...tuit,
+                   likes: tuit.likes + 1
+                 })} className="far fa-thumbs-up ms-2 "></i>
+             </div>
+             <div className="d-inline " >
+                 {tuit.dislikes}
+                  <i onClick={() => updateTuit(dispatch, {
+                    ...tuit,
+                    dislikes: tuit.dislikes + 1
+                  })} className="far fa-thumbs-down ms-2"></i>
+              </div>
         </span>
 
-
-        <i className="wd-fourth fa fa-upload"></i>
-
-    </div>
-  );
-
-}else{
-
-return (
-    <div className ="mx-auto wd-icons">
-        <span className="wd-first">
-            <i className=" fa fa-comment"></i>
-            {tuit.comments}
-        </span>
-        <span className="wd-second">
-             <i className="  fa fa-retweet"></i>
-             {tuit.retuits}
-        </span>
-
-        <span className="wd-third" onClick={() => updateTuit(dispatch, {
-                                                  ...tuit,
-                                                  likes: tuit.likes - 1,
-                                                  liked : false})}>
-        {
-          tuit.liked &&
-          <i className=" fas fa-heart me-1"
-             style={{color: 'red'}}></i>
-        }
-        {
-          !tuit.liked &&
-          <i className="far fa-heart me-1"></i>
-        }
-        {tuit && tuit.likes}
-        </span>
 
 
         <i className="wd-fourth fa fa-upload"></i>
@@ -76,10 +45,11 @@ return (
   );
 
 
-
-
-}
 
 }
 export default TuitStats;
+
+
+
+
 
